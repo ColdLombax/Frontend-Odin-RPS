b3362c69d4834a0009029529e3289d7c3b6f7fae
coldlombax <coldlombax@gmail.com>
2021-06-01 12:31:18 +0100

    Scoreboard component added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import ScoreBoard from "./components/ScoreBoard";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='pt-10 p-5 flex flex-col items-center'>
+      <ScoreBoard />
+      {/* <div className='w-24 h-24 bg-red-600'></div> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ScoreBoard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ScoreBoard() {
+  return (
+    <div
+      id='score-board'
+      className=' text-xl flex justify-center items-center pl-4 p-5 border-4 border-gray-400 rounded-xl w-full max-w-2xl sm:text-4xl'
+    >
+      <h1 className='leading-none'>
+        Rock
+        <br />
+        Paper
+        <br />
+        Scissors
+      </h1>
+      <section className='ml-auto bg-gray-100 p-2 text-center rounded-xl text-gray-600 w-1/3 h-full flex flex-col items-center justify-center sm:w-1/4'>
+        <h3 className='text-blue-700 text-xs sm:text-lg'>SCORE</h3>
+        <p className='text-4xl sm:text-6xl'>12</p>
+      </section>
+    </div>
+  );
+}
+
+export default ScoreBoard;
