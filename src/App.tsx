9ad082e662819ca33d8ce9a279d0fd486cc5ac3c
coldlombax <coldlombax@gmail.com>
2021-06-01 13:03:53 +0100

    Added Choice component as template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import ChoiceButton from "./components/ChoiceButton";
 import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
   return (
     <div className='pt-10 p-5 flex flex-col items-center'>
       <ScoreBoard />
-      {/* <div className='w-24 h-24 bg-red-600'></div> */}
+      <div className='w-full max-w-2xl grid'>
+        <ChoiceButton />
+      </div>
     </div>
   );
 }
