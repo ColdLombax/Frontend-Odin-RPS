efafa73efcd6e53cd054ced0828b1b81a513adf2
coldlombax <coldlombax@gmail.com>
2021-06-03 11:46:36 +0100

    Game over message and replay.

---
+++ src/components/GameOver.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type PropTypes = {
+  message: string;
+};
+
+function GameOver({ message }: PropTypes) {
+  return (
+    <div className='absolute left-0 bottom-20 w-full flex flex-col justify-center items-center p-5 lg:relative'>
+      <h2 className='text-2xl mb-2 sm:text-6xl sm:mb-5'>{message}</h2>
+      <p className='bg-white text-gray-700 text-md pl-10 pr-10 p-2 rounded-xl sm:pl-14 sm:pr-14 sm:p-4'>
+        PLAY AGAIN
+      </p>
+    </div>
+  );
+}
+
+export default GameOver;
