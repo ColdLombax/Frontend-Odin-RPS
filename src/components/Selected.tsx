3d56b8bac3e50a607c734de70c6876869afcb830
coldlombax <coldlombax@gmail.com>
2021-06-03 11:46:49 +0100

    Computer working with temp game over state.

--- src/components/Selected.tsx
+++ src/components/Selected.tsx
@@ -1,25 +1,45 @@
 import React, { useEffect, useState } from "react";
 
 import ChoiceButton from "./ChoiceButton";
+import GameOver from "./GameOver";
 
 type PropTypes = {
   playerChoice: string;
+  setGameState: any;
+  isGameOver: boolean;
 };
 
-function Selected({ playerChoice }: PropTypes) {
+function Selected({ playerChoice, setGameState, isGameOver }: PropTypes) {
   const [computerChoice, setComputerChoice] = useState("");
 
   const handleComputerChoice = () => {
-    const value = Math.floor(Math.random() * 2);
-    setComputerChoice("paper");
+    const value = Math.floor(Math.random() * 3);
+    switch (value) {
+      case 0:
+        setComputerChoice("paper");
+        break;
+      case 1:
+        setComputerChoice("rock");
+        break;
+      case 2:
+        setComputerChoice("scissors");
+        break;
+    }
   };
 
   useEffect(() => {
     setTimeout(handleComputerChoice, 1000);
   }, []);
 
+  useEffect(() => {
+    if (computerChoice) {
+      console.log(computerChoice);
+      console.log(playerChoice);
+    }
+  }, [computerChoice]);
+
   return (
-    <div className='flex gap-5 mt-10 sm:gap-60'>
+    <div className='flex gap-5 mt-10 sm:gap-40 sm:'>
       <div className='flex flex-col-reverse items-center gap-5 sm:text-2xl sm:flex-col'>
         <h2>YOU PICKED</h2>
         <ChoiceButton
@@ -33,6 +53,7 @@ function Selected({ playerChoice }: PropTypes) {
           }
         />
       </div>
+      {isGameOver && <GameOver message='hello' />}
       <div className='flex flex-col-reverse items-center gap-5 sm:text-2xl sm:flex-col'>
         <h2>THE HOUSE PICKED</h2>
         {computerChoice ? (
