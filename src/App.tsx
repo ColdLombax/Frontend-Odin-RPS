c3d00e8ec0c4e4f3a7852a4b0c2a20f8f7994be2
coldlombax <coldlombax@gmail.com>
2021-06-03 08:59:44 +0100

    Chain added with responsive cover.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import ChoiceButton from "./components/ChoiceButton";
 import ScoreBoard from "./components/ScoreBoard";
 
+import "./App.css";
+
 function App() {
   return (
     <div className='pt-10 p-5 flex flex-col items-center'>
       <ScoreBoard />
-      <div className='w-full max-w-2xl grid'>
-        <ChoiceButton choice='rock' />
-        <ChoiceButton choice='scissors' />
-        <ChoiceButton choice='paper' />
+      <div
+        id='board'
+        className='w-full max-w-2xl grid grid-flow-row grid-cols-2 place-items-center gap-20'
+      >
+        <ChoiceButton span={false} choice='rock' color='border-indigo-500' />
+        <ChoiceButton
+          span={false}
+          choice='scissors'
+          color='border-yellow-500'
+        />
+        <ChoiceButton span={true} choice='paper' color='border-red-500' />
       </div>
     </div>
   );
